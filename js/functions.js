e0ab549c53b879cfaf1d319140cc79024e88b9e2
piers55 <apiers55@gmail.com>
2014-11-03 22:51:34 -0600

    update functions

--- js/functions.js
+++ js/functions.js
@@ -19,23 +19,12 @@
 			$(".menu-desktop").toggleClass("menu-down-mobile");
 		});
 
-<<<<<<< HEAD
-	$(function(loadContent){
-		$(".load-home").click(function() {
-			$(".menu-desktop").removeClass("menu-top").addClass("menu-bottom").css({"transition" : "ease all 1s"});
-			$(".show-home").css({"transition" : "ease all 1s"});
-			$(".content-p").css({"top":"85vh","transition":"ease all 1s"});
-			$(".carousel-fade").css({"opacity":"1","transition":"ease all .8s"});
-		});
-		$(".load-acerca").click(function() {
-			$(".content-p").load("acerca.html #acerca-container");
-=======
+
 		$(".load-tienda").click(function() {
 			$(".js-to-load").load("tienda.html #tienda-container");
 		});
 		$(".load-tienda").click(function() {
 			$(".js-to-load").load("tienda.html #tienda-container");
->>>>>>> 9a74f9b49aea5cabf080175e00c9cd9e02a7a1a6
 		});
 		$(".load-tienda").click(function() {
 			$(".js-to-load").load("tienda.html #tienda-container");
