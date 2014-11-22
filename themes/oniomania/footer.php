4942444c3869a14cd198f5ba15edc76798dd78ba
piers55 <apiers55@gmail.com>
2014-11-21 19:05:58 -0600

    footer

--- themes/oniomania/footer.php
+++ themes/oniomania/footer.php
@@ -1,7 +1,12 @@
-		<!-- /.container -->
-		<?php wp_footer(); ?>
-		<footer>
-
-		</footer>
+			<!-- /.container -->
+			<?php wp_footer(); ?>
+			<footer>
+				<div class="[ row ]">
+					<div class=" [ center block ] [ text-center ]">
+						<a href="">Aviso de Privacidad</a>
+						<a href="">FAQ</a>
+					</div>
+				</div>
+			</footer>
     </body>
 </html>
