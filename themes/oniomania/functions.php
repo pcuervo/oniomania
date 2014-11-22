4195b41564fd8a1d476e0b89cefb716ddda2e5db
raul <raul@zamacona.me>
2014-11-21 18:26:18 -0600

    ajax y pages

--- themes/oniomania/functions.php
+++ themes/oniomania/functions.php
@@ -28,6 +28,7 @@
 
 		// localize scripts
 		wp_localize_script( 'functions', 'ajax_url', admin_url('admin-ajax.php') );
+		wp_localize_script( 'functions', 'site_url', site_url('/') );
 
 		// styles
 		wp_enqueue_style( 'styles', get_stylesheet_uri() );
